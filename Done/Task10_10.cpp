3073fcf740e2a43d0ac1a672f0f5d772e2f5f68d
Ducky <kriperoka@gmail.com>
2023-12-08 09:38:52 +0300

    task 13 added

--- Done/Task10_10.cpp
+++ Done/Task10_10.cpp
@@ -10,7 +10,7 @@ int main() {
 
 	for (int i = 2; i < n; i++) {
 
-		if (n % i == 0) {
+		if (n % i == 0) { 
 
 			cout << i << " ";
 
--- Main/Main.cpp
+++ Main/Main.cpp
@@ -6,29 +6,9 @@
 
 using namespace std;
 
-void toBit(int x) {
-
-	vector<int> bits;
-
-	while (x > 1)
-	{
-		if (x % 2 == 0) { bits.push_back(0); }
-		else { bits.push_back(1); }
-		x /= 2;
-	}
-	bits.push_back(1);
-	reverse(bits.begin(), bits.end());
-
-	for (int bit : bits) {
-		cout << bit;
-	}
-}
-
 int main() {
+	
+	
 
-	int x, y;
-	cin >> x;
-
-	toBit(x);
 	return 0;
 }
