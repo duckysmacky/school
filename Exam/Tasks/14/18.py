664d469015221802603e4fde26181fbbf6e23408
Ducky <kriperoka@gmail.com>
2024-03-01 17:28:10 +0300

    exam task 14_2

---
+++ Exam/Tasks/14/18.py
@@ -0,0 +1,16 @@
+def f(x: int, base: int):
+    num = []
+    while x > 0:
+        num.append(x % base)
+        x = x // base
+    return "".join((map(str, num[::-1])))
+
+
+m = 10 ** 10
+
+for i in range(2, 32):
+    if len(str(f(50, i))) == 2:
+        m = min(m, i)
+
+print(m)
+# 8
