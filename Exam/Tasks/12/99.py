01dde345900296d9641df651eeb5d7873bd615b7
Ducky <kriperoka@gmail.com>
2024-04-12 11:34:06 +0300

    addd task 23

--- Exam/Tasks/12/99.py
+++ Exam/Tasks/12/99.py
@@ -1,1 +1,21 @@
-# TODO
+def add(i, x, y):
+    i[0] += x
+    i[1] += y
+    return i
+
+c = 0
+start = [0, 0]
+ans = []
+for a in range(-1000, 1000):
+    for b in range(-1000, 1000):
+        t = start.copy()
+        add(t, -3, 4)
+        for i in range(4):
+            add(t, a, b)
+            add(t, 12, 5)
+        add(t, -9, 32)
+        if t == start:
+            ans = [a, b]
+            break
+        
+print(c)
