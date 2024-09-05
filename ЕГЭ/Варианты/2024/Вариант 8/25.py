1ad556dcf38bf7a84abe76f2713e967c03b5dbf8
DuckySmacky <kriperoka@gmail.com>
2024-09-05 11:16:29 +0300

    variant 8 answers checke

--- ЕГЭ/Варианты/2024/Вариант 8/25.py
+++ ЕГЭ/Варианты/2024/Вариант 8/25.py
@@ -1,32 +1,18 @@
-number_range = range(163461, 164423 + 1)
+number_range = range(164361, 164423 + 1)
 test_range = range(50, 54 + 1)
 ans = []
 
 
 def find_pair(x: int) -> (int, int):
-    count = 0
-    pair = ()
-
-    for a in range(1, x + 1):
-        b = x - a
-        while a <= b:
-            print(a, b)
+    pairs = set()
+    for a in range(1, int(x ** 0.5) + 1):
+        for b in range(a, int(x ** 0.5) + 1):
             if a ** 2 + b ** 2 == x:
-                count += 1
-                pair = a, b
-                if count > 1:
-                    return 0, 0
-            b -= 1
-
-    if count == 1:
-        return pair
-    else:
-        return 0, 0
+                if (a, b) not in pairs:
+                    pairs.add((a, b))
+    if len(pairs) == 1:
+        print(pairs.pop())
 
 
 for n in number_range:
-    pair = find_pair(n)
-    if pair != (0, 0):
-        ans.append(pair)
-
-for p in ans: print(p)
+    find_pair(n)
