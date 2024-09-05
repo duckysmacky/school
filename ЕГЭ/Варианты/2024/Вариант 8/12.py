46dd066038c4712636727661d43872c4d1076552
DuckySmacky <kriperoka@gmail.com>
2024-09-05 10:32:29 +0300

    fixed variant 8

--- ЕГЭ/Варианты/2024/Вариант 8/12.py
+++ ЕГЭ/Варианты/2024/Вариант 8/12.py
@@ -4,8 +4,8 @@ while ">1" in x or ">4" in x or ">6" in x:
     if ">1" in x:
         x = x.replace(">1", "1661>", 1)
     if ">4" in x:
-        x = x.replace(">4", "146141", 1)
+        x = x.replace(">4", "146141>", 1)
     if ">6" in x:
         x = x.replace(">6", "141>", 1)
 
-print(sum([int(n) for n in x]))
+print(sum([int(n) for n in x if n.isdigit()]))
