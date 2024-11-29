01cde37efd470da8c807357b2f08527de01534cd
DuckySmacky <kriperoka@gmail.com>
2024-11-29 19:16:34 +0300

    Задание 25 вариант 8

--- ЕГЭ/Варианты/2023/Вариант 8/25.py
+++ ЕГЭ/Варианты/2023/Вариант 8/25.py
@@ -1,7 +1,14 @@
-def sq_sum(x: int) -> tuple:
-    pass
-
-
+def sq_sum(x: int) -> tuple | None:
+    ab = ()
+    for div in range(1, int(x ** 0.5) + 1):
+        a, b = div, x // div
+        if a ** 2 + b ** 2 == x:
+            if ab != ():
+                return None
+            ab = (a, b)
+    return ab
 
 for num in range(164_361, 164_423 + 1):
-    pass
+    sqs = sq_sum(num)
+    if sqs != None and sqs != ():
+        print(sqs)
