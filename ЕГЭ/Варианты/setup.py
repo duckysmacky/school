f187e11522d52a0b1f4bdd9c734b4f224e8d8274
DuckySmacky <kriperoka@gmail.com>
2024-12-19 16:23:01 +0300

    Закончил вариант 13

--- ЕГЭ/Варианты/setup.py
+++ ЕГЭ/Варианты/setup.py
@@ -14,7 +14,7 @@ answers_file = open(f"{dir}/!answers.txt", 'w')
 for i in range(27):
     answers_file.write(f"{i + 1} - {'\n' if i < 26 else ''}")
 
-python_files = [2, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 23, 24, 25, 26]
-for file in map(lambda x: f"{x}.py", python_files):
-    f = open(f"{dir}/{file}", 'w')
-    f.close()
+# python_files = [2, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 23, 24, 25, 26]
+# for file in map(lambda x: f"{x}.py", python_files):
+#     f = open(f"{dir}/{file}", 'w')
+#     f.close()
