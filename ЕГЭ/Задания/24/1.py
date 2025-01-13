1482e79642ef985dc901b062539724ddc33ac860
DuckySmacky <kriperoka@gmail.com>
2025-01-13 15:17:38 +0300

    Сделал задание 24 на цифры

---
+++ ЕГЭ/Задания/24/1.py
@@ -0,0 +1,18 @@
+file = open("1.txt")
+text = file.readline().replace('-', '+')
+
+max_val = ''
+val = ''
+for num in text.split('+'):
+    if len(num) > 0:
+        if num[0] != '0' or num == '0':
+            val += num + '+'
+        elif num[0] == '0':
+            val += "0+"
+            max_val = max(max_val, val, key=len)
+            val = str(int(num)) + '+'
+    else:
+        val = ''
+    max_val = max(max_val, val, key=len)
+print(max_val)
+print(len(max_val) - 1)
