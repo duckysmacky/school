6e378b48bf8e572b6cd61bce52d792b53fd55eb0
DuckySmacky <kriperoka@gmail.com>
2025-03-10 23:21:23 +0300

    Закончил вариант 16 2025

--- ЕГЭ/Варианты/2025/Вариант 16/11.py
+++ ЕГЭ/Варианты/2025/Вариант 16/11.py
@@ -4,4 +4,4 @@ k = 158
 N = 10 + 2022
 i = ceil(log2(N))
 I = ceil(k * i / 8)
-print(ceil(I * 15_360 / (1024 * 8)))
+print(ceil(I * 15_360 / 1024))
--- ЕГЭ/Варианты/2025/Вариант 16/6.py
+++ ЕГЭ/Варианты/2025/Вариант 16/6.py
@@ -4,17 +4,17 @@ tracer(0)
 screensize(10000, 10000)
 k = 20
 left(90)
-penup()
 
+penup()
 forward(100 * k)
 right(90)
 forward(100 * k)
 right(30)
 pendown()
 for _ in range(10):
-    forward(30 * k)
+    forward(30 / 10 * k)
     right(90)
-    forward(40 * k)
+    forward(40 / 10 * k)
     right(90)
 
 penup()
