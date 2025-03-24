b50a085c234690a8b84f509e1b244715790bdac2
DuckySmacky <kriperoka@gmail.com>
2025-03-24 19:40:45 +0300

    Закончил вариант 18 2025

--- ЕГЭ/Варианты/2025/Вариант 18/24.py
+++ ЕГЭ/Варианты/2025/Вариант 18/24.py
@@ -1,10 +1,10 @@
 file = open('24.txt')
 text = file.readline()
-text = 'AA***AA**A**A***A***A*****A***A**A***A**A'
+# text = 'A*****A***A****A*****A**A***A***A*****A***A**A***A**A'
 
 indexes = [i for i in range(len(text)) if text[i] == 'A']
-step = 2 + 1
+step = 2024 - 1
 cnts = []
 for i in range(len(indexes) - step):
-    cnts.append(indexes[i + step] - indexes[i])
+    cnts.append(indexes[i + step] - indexes[i] + 1)
 print(min(cnts))
